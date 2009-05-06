36602f47319c7a92b37d04fa6b2bd12253bcd807
whit <whit@openplans.org>
2009-05-06 15:38:58 -0500

    paver boilerplate

--- pavement.py
+++ pavement.py
@@ -18,3 +18,53 @@ from setuptools import find_packages
 import os
 
 setuputils.install_distutils_tasks()
+
+version = "0.0"
+
+
+setup(name='nunatak',
+      version=version,
+      description="OpenGeo Stack Builder",
+      long_description="""
+      Tools for building and managing an opengeo stack
+      """,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='whit',
+      author_email='whit@opengeo.org',
+      url='http://www.opengeo.org',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      dependency_links=[],
+      install_requires=["sphinx>=0.6.1",
+                        "PasteDeploy",
+                        "Paste",
+                        "PasteScript",
+                        "JSTools>=0.1.2",
+                        ],
+      entry_points="""
+      """,
+      )
+
+curdir = os.path.abspath(os.curdir)
+options(
+    virtualenv=Bunch(script_name="build_stack",
+                     packages_to_install=[],
+                     paver_command_line="after_bootstrap"
+                     ),
+#    sphinx=Bunch(docroot="src/trunk/docsrc",
+#                 builddir=path(curdir) / "built")
+    )
+
+@task
+def auto():
+    cp = ConfigParser()
+    #@@ make an option??
+    cp.read("stack-conf.cfg")
+    options(config=cp)
+
+@task
+def build_stack():
+    info("Not Implemented yet")
--- setup.py
+++ setup.py
@@ -1,26 +1,12 @@
-from setuptools import setup, find_packages
-import sys, os
+import os
+try:
+    import paver
+except ImportError:
+    if os.path.exists("paver-minilib.zip"):
+        import sys
+        sys.path.insert(0, "paver-minilib.zip")
+    else:
+        raise
 
-version = '0.1'
-
-setup(name='nunatak',
-      version=version,
-      description="a catch all for creating an environment for doing GIS work",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='GIS',
-      author='whit',
-      author_email='whit@opengeo.org',
-      url='http://docs.geospiel.org',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+import paver.tasks
+paver.tasks.main()

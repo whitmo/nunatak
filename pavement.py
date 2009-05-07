d57bbcf5238c862a573fd711196e96bba9fcb8d6
whit <whit@openplans.org>
2009-05-07 09:21:15 -0500

    fix pipe for unpack, setup to download all first

--- pavement.py
+++ pavement.py
@@ -1,3 +1,5 @@
+from __future__ import with_statement
+
 try:
     from paver.virtual import bootstrap
 except :
@@ -10,12 +12,14 @@ from paver.easy import *
 from paver.easy import call_task
 from paver.easy import call_task #debug,
 from paver.easy import cmdopts #,consume_args
-from paver.easy import path, sh, info
+from paver.easy import path, sh, info, pushd
 from paver.easy import task, options, Bunch
 from paver.setuputils import setup
 from paver.tasks import help, needs
 from setuptools import find_packages
+import subprocess
 import os
+import shutil
 
 setuputils.install_distutils_tasks()
 
@@ -60,11 +64,80 @@ options(
 
 @task
 def auto():
+    env = os.environ['VIRTUAL_ENV']
     cp = ConfigParser()
     #@@ make an option??
     cp.read("stack-conf.cfg")
-    options(config=cp)
+    options(config=cp,
+            env=env)
 
 @task
 def build_stack():
     info("Not Implemented yet")
+
+
+
+def download(url, dest=".download"):
+    filename = url.split("/")[-1]
+    ppath = path(dest)
+
+    fpath = ppath / filename
+    if not (ppath / filename).exists():
+        oldnames = set(ppath.listdir())
+        with pushd(ppath) as old_dir:
+            sh("wget %s" %url)
+    return fpath
+
+def tarball_unpack(fpath, dest):
+    """
+    Dumbly unpack tarballs
+
+    fpath --
+       filepath of tarball
+
+    dest --
+       folder to upack into
+
+    @return
+        name of folder created by unpacking
+    """
+    dest = path(dest)
+    filename = fpath.split("/")[-1]
+    newfile = dest / filename
+    old = set(os.listdir(dest))
+    shutil.copyfile(str(fpath), str(newfile))
+    with pushd(dest):
+        catcmd = "zcat"
+        if filename.endswith("bz2") or filename.endswith("bz"):
+            catcmd = "bzcat"
+        cat = subprocess.Popen([catcmd, filename], stdout=subprocess.PIPE)
+        untar = subprocess.Popen(["tar", "-xf", "-"], stdin=cat.stdout, stdout=subprocess.PIPE)
+        info("Unpacking %s" %filename)
+        untar.communicate()
+    os.remove(newfile)
+    return dest / filename.split(".tar.")[0]
+
+# paver needs a "finish" to right out the stack-config
+
+@task
+@needs(['dir_layout'])
+def get_sources():
+    for pkg in options.config.options("urls"):
+        url = options.config.get("urls", pkg)
+        if url.startswith("ftp") or url.startswith("http"):
+            tarball = download(url)
+            source_path = tarball_unpack(tarball, "src")
+        else:
+            # @@add svn checkup when needed
+            pass
+        options.config.set("sources", pkg, str(source_path))
+        
+@task
+def dir_layout():
+    env = path(options.env)
+    for p in "src", "var", "etc", ".download",:
+        if not (env / p).exists():
+            os.mkdir(env / p)
+
+
+

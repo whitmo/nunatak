1853c4e63b39d2955e7fa15fb61bfb7da08cecd2
whit <whit@openplans.org>
2009-05-22 19:10:37 -0500

    basic db setup working... still no adduser yet, but should be fine for testing with folks who already have a pg user

--- pavement.py
+++ pavement.py
@@ -9,7 +9,6 @@ except :
 from ConfigParser import ConfigParser, NoOptionError
 from paver import setuputils
 from paver.easy import *
-from paver.easy import call_task
 from paver.easy import call_task #debug,
 from paver.easy import cmdopts #,consume_args
 from paver.easy import path, sh, info, pushd
@@ -21,6 +20,8 @@ import os
 import re
 import shutil
 import subprocess
+import getpass
+import pwd
 
 setuputils.install_distutils_tasks()
 
@@ -63,24 +64,108 @@ options(
 #                 builddir=path(curdir) / "built")
     )
 
+DEF_CONF = "stack-conf.cfg"
+_c_base = 'proj', 'gdal', 'geos'
+
 @task
 def auto():
-    env = os.environ['VIRTUAL_ENV']
+    env = os.environ.get('VIRTUAL_ENV')
+    if env is None:
+        env = path("./").abspath()
     cp = ConfigParser()
+
     #@@ make an option??
-    cp.read("stack-conf.cfg")
+    cp.read(DEF_CONF)
     options(config=cp,
-            env=env)
+            env=env,
+            conf_fp=DEF_CONF)
 
 @task
 @needs(['auto'])
 def after_bootstrap():
-    info("not implemented")
+    info("under construction")
+    call_task("install_c_base")
+    call_task("install_postgis")
+    call_task("save_cfg")
+
+
+@task
+@needs(['auto'])
+def save_cfg():
+    options.config.write(open(options.conf_fp, 'w'))
+
 
 @task
 def build_stack():
     info("Not Implemented yet")
 
+
+@task
+@needs(['get_sources'])
+def install_c_base():
+    for pkg in _c_base:
+        basic_install(pkg)
+
+
+        
+@task
+@needs(['get_sources'])
+def install_postgis():
+    # @@ make configure optional?
+    venv = options.env
+    basic_install("readline")
+    basic_install("postgres")
+    pg_config = path(venv) / "bin/pg_config"
+    basic_install("postgis", "--with-pgsql=%s" %pg_config)
+    if  get_option(options.config, "installed", "gdal2") is None:
+        basic_install("gdal", "--with-pg=%s" %pg_config, force=True)
+        options.config.set("installed", "gdal2", True)
+        call_task("save_cfg")
+    pg_after_install()
+        
+
+# paver needs a "finish" autocall to write out the stack-config
+
+
+ZIP = re.compile(r'.download/(.+)\.zip')
+
+def zipfilename(dpath):
+    # @@ maybe expand to get_filename
+    return ZIP.match(dpath).groups()[0]
+
+_irregulars = {'geoserver-1.7.4-bin':'geoserver-1.7.4'}
+
+@task
+@needs(['dir_layout'])
+def get_sources():
+    for pkg in options.config.options("urls"):
+        url = options.config.get("urls", pkg)
+        src = path("./src")
+        if url.startswith("ftp") or url.startswith("http"):
+            tarball = download(url)
+            if tarball.endswith("zip"):
+                filename = zipfilename(tarball)
+                filename = _irregulars.get(filename, filename)
+                if not (src / filename).exists():
+                    sh("unzip %s -d %s" %(tarball, src))
+            else:
+                source_path = tarball_unpack(tarball, src)
+        else:
+            # @@add svn checkup when needed
+            pass
+        options.config.set("sources", pkg, source_path.abspath())
+    lpath = path("src/LICENSE.txt")
+    if lpath.exists():
+        lpath.remove()
+
+_dirs = "src", "var", "etc", ".download", "var/logs"
+        
+@task
+def dir_layout():
+    env = path(options.env)
+    for p in _dirs:
+        if not (env / p).exists():
+            os.mkdir(env / p)
 
 
 def download(url, dest=".download"):
@@ -93,6 +178,7 @@ def download(url, dest=".download"):
         with pushd(ppath) as old_dir:
             sh("wget %s" %url)
     return fpath
+
 
 def tarball_unpack(fpath, dest, overwrite=False):
     """
@@ -126,77 +212,76 @@ def tarball_unpack(fpath, dest, overwrite=False):
         os.remove(newfile)
     return dest_folder
 
-def basic_install(pkg, extra2="", config=None, clean=False):
-    venv = os.environ['VIRTUAL_ENV']
+
+def get_option(config, section, opt, default=None):
+    try:
+        return config.get(section, opt)
+    except NoOptionError:
+        return default
+
+
+def basic_install(pkg, extra2="", config=None, clean=False, force=False):
+    # use config.status to determine whether to rerun?
+    venv = options.env
     if config is None:
         config = options.config
     src = path(config.get('sources', pkg))
-    try:
-        extra = config.get('install_options', pkg)
-    except NoOptionError:
-        extra = ""
-    with pushd(src) as root:
-        sh("configure LDFLAGS=-L%s CPPFLAGS=-I%s --prefix=%s %s %s" %(str(path(venv) / 'lib'), str(path(venv) / 'include'), venv, extra, extra2))
-        if clean:
-            sh("make clean")
-        sh("make")
-        sh("make install")
+    extra = get_option(config, 'install_options', pkg, "")
+    if force is True:
+        config.remove_option('installed', pkg)
+    if get_option(config, "installed", pkg) is None:
+        with pushd(src) as root:
+            sh("configure LDFLAGS=-L%s CPPFLAGS=-I%s --prefix=%s %s %s" %(str(path(venv) / 'lib'), str(path(venv) / 'include'), venv, extra, extra2))
+            if clean:
+                sh("make clean")
+            sh("make")
+            sh("make install")
+            config.set('installed', pkg, True)
+            call_task("save_cfg")
 
-@task
-@needs(['get_sources'])
-def install_c_base():
-    for pkg in 'proj', 'gdal', 'geos':
-        basic_install(pkg)
-
-@task
-@needs(['get_sources'])
-def install_postgis():
-    # @@ make configure optional?
-    venv = os.environ['VIRTUAL_ENV']
-    basic_install("readline")
-    basic_install("postgres")
-    pg_config = path(venv) / "bin/pg_config"
-    basic_install("postgis", "--with-pgsql=%s" %pg_config)
-    basic_install("gdal", "--with-pg=%s" %pg_config) 
-
-# paver needs a "finish" autocall to write out the stack-config
-
-
-ZIP = re.compile(r'.download/(.+)\.zip')
-
-def zipfilename(dpath):
-    # @@ maybe expand to get_filename
-    return ZIP.match(dpath).groups()[0]
-
-
-@task
-@needs(['dir_layout'])
-def get_sources():
-    for pkg in options.config.options("urls"):
-        url = options.config.get("urls", pkg)
-        src = path("./src")
-        if url.startswith("ftp") or url.startswith("http"):
-            tarball = download(url)
-            if tarball.endswith("zip"):
-                filename = zipfilename(tarball)
-                if not (src / filename).exists():
-                    sh("unzip %s -d %s" %(tarball, src))
-            else:
-                source_path = tarball_unpack(tarball, src)
-        else:
-            # @@add svn checkup when needed
-            pass
-        options.config.set("sources", pkg, str(source_path))
-    lpath = path("src/LICENSE.txt")
-    if lpath.exists():
-        lpath.remove()
         
-@task
-def dir_layout():
-    env = path(options.env)
-    for p in "src", "var", "etc", ".download",:
-        if not (env / p).exists():
-            os.mkdir(env / p)
+def add_pg_user():
+    if sys.platform == 'darwin':
+        info("osx user add not implemented yet")
+        # http://osxdaily.com/2007/10/29/how-to-add-a-user-from-the-os-x-command-line-works-with-leopard/
+##         Create a new entry in the local (/) domain under the category /users.
+##         dscl / -create /Users/toddharris
 
+##         Create and set the shell property to bash.
+##         dscl / -create /Users/toddharris UserShell /bin/bash
 
+##         Create and set the user's full name.
+##         dscl / -create /Users/toddharris RealName "Dr. Todd Harris"
 
+##         Create and set the users ID.
+##         dscl / -create /Users/toddharris UniqueID 503
+
+##         Create and set the users group ID property.
+##         dscl / -create /Users/toddharris PrimaryGroupID 1000
+
+##         Create and set the user home directory.
+##         dscl / -create /Users/toddharris NFSHomeDirectory /Local/Users/toddharris
+
+##         Set the password.
+##         dscl / -passwd /Users/toddharris PASSWORD
+    else:
+        info("*nix user add not implemented yet")
+    return pwd.getpwnam('postgres')[2]
+
+def pg_after_install():
+    # set up postgres user
+    try:
+        pguserid = pwd.getpwnam('postgres')[2]
+    except KeyError:
+        pguserid = add_pg_user()
+    vardir = path(options.env) / 'var'
+    pgdata = vardir / 'pgdata'
+    if not pgdata.exists():
+        try:
+            pgdata.mkdir()
+            subprocess.call("sudo chown postgres:postgres %s" %pgdata.abspath(), shell=True)
+            subprocess.call("sudo -u postgres bin/initdb -D %s" %pgdata.abspath(), shell=True)
+        except :
+            pgdata.rmdir()
+            raise
+    
